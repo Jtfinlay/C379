008846e65c8ac102ba7bb9bf8a1f103505694ee4
James Finlay <jtfinlay@ualberta.ca>
2013-10-26 11:00:03 -0600

    Initial push

---
+++ assn2/helpers.h
@@ -0,0 +1,253 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+	
+	
+	
+char * getIPString(struct sockaddr_in * client) {
+	char *buff, *tmp;
+	tmp = (char*) malloc(sizeof(char)*20);
+	if (tmp == NULL)
+		return NULL;
+	buff = tmp;
+	
+	sprintf(buff, "%d.%d.%d.%d\n",
+		(int)(client->sin_addr.s_addr&0xFF),
+		(int)((client->sin_addr.s_addr&0xFF00)>>8),
+		(int)((client->sin_addr.s_addr&0xFF0000)>>16),
+		(int)((client->sin_addr.s_addr&0xFF000000)>>24));
+//		(int)((*client.sin_addr.s_addr&0xFF000000)>>24));
+		
+	tmp = NULL;
+	return buff;
+}
+
+char* getTime() {
+	time_t t = time(NULL);
+	return asctime(gmtime(&t));
+}
+void internalError(struct sockaddr_in * client, char * error, char * get) {
+	char * ip;
+	sendGenError(client);
+	
+	if (get != NULL)
+		logInternal(getIPString(client), get);
+	err(1, error);
+}
+/* Returns characters sent */
+int writeToClient(int clientsd, char * buff) {
+	ssize_t w, written;
+
+	w = 0;
+	written = 0;
+	while (written < strlen(buff)) {
+		w = write(clientsd, buff + written,
+			strlen(buff) - written);
+		if (w == -1) {
+			if (errno != EINTR)
+				return written;
+		} else
+			written += w;
+	}
+	return written;
+}
+int checkGET(char * buff, char * fileName) {
+	char *word, *line, *lptr, *wptr;
+
+	wptr = NULL;
+	lptr = NULL;
+	
+	printf("%s\n", buff);
+	
+	/* First line should be 'GET /someplace/file.html HTTP/1.1' */
+	
+	line = strtok_r(buff, "\n", &lptr);
+	
+	word = strtok_r(line, " ", &wptr);
+	if (word == NULL || strncmp(word, "GET", 3) != 0)
+		return 0;
+	
+	word = strtok_r(NULL, " ", &wptr);
+	if (word == NULL)
+		return 0;
+	strlcpy(fileName, word);
+	
+	word = strtok_r(NULL, " ", &wptr);
+	if (word == NULL || strncmp(word, "HTTP/1.1",  8) != 0)
+		return 0;	
+	
+	/* Ensure there is a blank line */
+	line = strtok_r(NULL, "\n", &lptr);
+	while (line != NULL) {
+		if (strlen(line) == 1) /* 1 because of '\0' in strtok */
+			return 1;
+		line = strtok_r(NULL, "\n", &lptr);
+	}
+	printf("No newline!\n");
+	return 0;
+}
+void sendOK(int clientsd, int fileLen) {
+	char buf[128], length[20];
+	
+	sprintf(length, "%d", fileLen);
+	
+	printf("OK!\n");
+	strlcpy(buf, "HTTP/1.1 200 OK\n");
+	strcat(buf, "Date: ");
+	strcat(buf, getTime());
+	strcat(buf, "Content-Type: text/html\n");
+	strcat(buf, "Content-Length: ");
+	strcat(buf, length);
+	strcat(buf, "\n\n");
+	
+	writeToClient(clientsd, buf);
+}
+void sendError(int clientsd, char * title, char * content) {
+	char *buf, *tmp, length[20];
+	tmp = (char*) malloc((128+strlen(content))*sizeof(char));
+	if (tmp == NULL)
+		err(1, "malloc");
+	
+	buf = tmp;
+	
+	sprintf(length, "%d", strlen(content));
+	
+	strlcpy(buf, "HTTP/1.1 ");
+	strcat(buf, title);
+	strcat(buf, "Date: ");
+	strcat(buf, getTime());
+	strcat(buf, "Content-Type: text/html\n");
+	strcat(buf, "Content-Length: ");
+	strcat(buf, length);
+	strcat(buf, "\n\n");
+	strcat(buf, content);
+	
+	writeToClient(clientsd, buf);
+	
+	free(buf);
+	buf=NULL;
+	tmp=NULL;
+}
+void sendBadRequestError(int clientsd) {
+	char title[30], content[128];
+	
+	printf("Bad Request Error\n");
+	
+	strlcpy(title, "400 Bad Request\n");
+	strlcpy(content, "<html><body>\n");
+	strcat(content, "<h2>Malformed Request</h2>\n");
+	strcat(content, "Your browser sent a request I could not understand\n");
+	strcat(content, "</body></html>\n");
+	
+	sendError(clientsd, title, content);
+}
+void sendNotFoundError(int clientsd) {
+	char title[30], content[128];
+	
+	printf("Not Found Error\n");
+	
+	strlcpy(title, "404 Not Found\n");
+	strlcpy(content, "<html><body>\n");
+	strcat(content, "<h2>Document not found</h2>\n");
+	strcat(content, "You asked for a document that doesn't exist.\n");
+	strcat(content, "</body></html>\n");
+	
+	sendError(clientsd, title, content);
+}
+void sendForbiddenError(int clientsd) {
+	char title[30], content[128];
+	
+	printf("Forbidden Error\n");
+	
+	strlcpy(title, "403 Forbidden\n");
+	strlcpy(content, "<html><body>\n");
+	strcat(content, "<h2>Permission denied</h2>\n");
+	strcat(content, "You cannot see this document.\n");
+	strcat(content, "</body></html>\n");
+	
+	sendError(clientsd, title, content);
+}
+void sendGenError(int clientsd) {
+	char title[30], content[128];
+	
+	printf("Generic Error\n");
+	
+	strlcpy(title, "500 Internal Server Error\n");
+	strlcpy(content, "<html><body>\n");
+	strcat(content, "<h2>Oops. That didn't work.</h2>\n");
+	strcat(content, "Something bad happened.\n");
+	strcat(content, "</body></html>\n");	
+
+	sendError(clientsd, title, content);
+}
+int sendFile(FILE * fp, int clientsd) {
+	int buffSize = 256;
+	int counter;
+	char buff[buffSize+1];
+	int r;
+	/* 
+	 * NOTE - use fseek to get file size
+	 * http://www.cplusplus.com/reference/cstdio/fread/
+	 */
+	memset(buff, '\0', (buffSize+1)*sizeof(char));
+	r = buffSize;
+	counter = 0;
+	while (r == buffSize) {
+		r = fread(buff, sizeof(char), buffSize, fp);
+		counter += writeToClient(clientsd, buff);
+		
+		memset(buff, '\0', buffSize*sizeof(char));
+	}		
+	return counter;	
+}
+
+
+
+
+
+
+void writeLog(char * ip, char * get, char * req) {
+	char buf[80+strlen(get)];
+	
+	strlcpy(buf, getTime());
+	strcat(buf, "\t");
+	strcat(buf, ip);
+	strcat(buf, "\t");
+	strcat(buf, get);
+	strcat(buf, "\t");
+	strcat(buf, req);
+	strcat(buf, "\n");
+	
+	printf("Writing: %s\n", buf);
+	
+	
+}
+void logOK(char * ip, char * get, int iWrote, int iTotal) {
+	char buf[40];
+	
+	sprintf(buf, "200 OK %d/%d", iWrote, iTotal); 
+	
+	writeLog(ip, get, buf);
+}
+void logBadRequest(char * ip, char * get) {
+	writeLog(ip, get, "400 Bad Request");
+}
+void logNotFound(char * ip, char * get) {
+	writeLog(ip, get, "404 Not Found");
+}
+void logForbidden(char * ip, char * get) {
+	writeLog(ip, get, "403 Forbidden");
+}
+void logInternal(char * ip, char * get) {
+	writeLog(ip, get, "500 Internal Server Error");
+}
---
+++ assn2/server_f.c
@@ -0,0 +1,178 @@
+/*
+ * Licensing goes here.
+ *
+ */
+
+/*
+ * Compile using something
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef HELPERS_H
+#define HELPERS_H
+	#include "helpers.h"
+#endif
+
+static void usage()
+{
+	extern char * __progname;
+	fprintf(stderr, "usage: %s portnumber dir logfile\n", __progname);
+	exit(1);
+}
+static void kidhandler(int signum) {
+	/* signal handler for SIGCHLD */
+	waitpid(WAIT_ANY, NULL, WNOHANG);
+}
+static void readSocket(int sockfd, char * buff, size_t bufflen) {
+	/* reads GET from socket */
+	if (recv(sockfd, buff, bufflen, 0) == -1)
+		err(1, "receive failed");	
+
+	// NOTE -> Might not buffer everything. Probably should fix that.
+}
+int main(int argc, char * argv[])
+{
+	struct sockaddr_in sockname, client;
+	struct sigaction sa;
+	char *ep, *inbuff, *tmp;
+	char outbuff[256], fName[256];
+
+	int clientlen, sd;
+
+	u_short port;
+	u_long p;
+	pid_t pid;
+
+	/* check params */
+	if (argc != 4)
+		usage();
+
+	errno = 0;
+	p = strtoul(argv[1], &ep, 10);
+	if (&argv[1] == '\0' || *ep != '\0'){
+		/* parameter wasn't a number, or was empty */
+		fprintf(stderr, "%s - not a number\n", argv[1]);
+		usage();
+	}
+	if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX)) {
+		/* It's a number, but it either can't fit in an unsigned
+		 * long, or it is too big for an unsigned short */
+	}
+	
+	/* Ensure argv[2] is a valid directory */
+	
+	if (chdir(argv[2]) == -1)
+		err(1, "chdir failed");
+		
+	/* Ensure argv[3] is a valid directory */
+
+	/* now safe to do this */
+	port = p;
+
+	memset(&sockname, 0, sizeof(sockname));
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(port);
+	sockname.sin_addr.s_addr = htonl(INADDR_ANY);
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sd == -1)
+		err(1, "socket failed");
+	if (bind(sd, (struct sockaddr *) &sockname, sizeof(sockname)) == -1)
+		err(1, "bind failed");
+	if (listen(sd, 3) == -1)
+		err(1, "listen failed");
+
+	/* We are now bound and listening for connections on "sd" */
+
+	/* Catch SIGCHLD to stop zombie children wandering around */
+	sa.sa_handler = kidhandler;
+	sigemptyset(&sa.sa_mask);
+
+	/* allow system calls (accept) to restart if interrupted by SIGCHLD */
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+		err(1, "sigaction failed");
+
+	/* main loop. Accept connections and do stuff */
+	printf("Server up and listening on port %u\n", port);
+	for(;;) {
+		int clientsd;
+		ssize_t written, w;
+	
+		clientlen = sizeof(&client);
+		clientsd = accept(sd, (struct sockaddr *)&client, &clientlen);
+		if (clientsd == -1)
+			err(1, "accept failed");
+		
+		/* fork child to deal with each connection */
+		pid = fork();
+		if (pid == -1)
+			internalError((struct sockaddr *)&client, "fork failed", NULL);		
+
+		if (pid == 0) {
+			int valid, written;
+			long lSize;
+			FILE * fp;
+			
+			/* Parse GET */
+			//outbuff = malloc(128*sizeof(char));
+			tmp = malloc(128*sizeof(char));
+			if (tmp == NULL)
+				internalError(&client, "malloc failed", NULL);
+			inbuff = tmp;
+			readSocket(clientsd, inbuff, 128);
+			
+			printf("Before: '%s'\n", inbuff);
+			valid = checkGET(inbuff, fName);
+			printf("After: '&s'\n", inbuff);
+			if (valid == 0) {
+				/* BAD REQUEST */
+				sendBadRequestError(clientsd);
+			} else if (valid == 1) {
+				/* GET is good. Try reading file & sending */
+				fp = fopen(fName, "r");
+				if (fp == NULL) {
+					if (errno == ENOENT) {
+						sendNotFoundError(clientsd);
+						logNotFound(getIPString(&client), inbuff);
+					} else if (errno == EACCES) {
+						sendForbiddenError(clientsd);
+						logForbidden(getIPString(&client), inbuff);
+					} else 
+						internalError(&client, "fopen failed", inbuff);
+				} else {
+					/* get file size */
+					fseek(fp, sizeof(char), SEEK_END);
+					lSize = ftell(fp);
+					rewind(fp);
+					/* send OK and file */
+					sendOK(clientsd, lSize);
+					written = sendFile(fp, clientsd);
+					logOK(getIPString(&client), inbuff);
+				}		
+			}
+
+			/* Clean up */
+			free(inbuff);
+			//free(outbuff);
+			inbuff = NULL;
+			//outbuff = NULL;
+
+			exit(0);
+		}
+		close(clientsd);
+	}
+}
+
