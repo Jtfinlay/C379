10f3137b10b1ab7af89087c513a51b6458f7e25a
James Finlay <jtfinlay@ualberta.ca>
2013-10-27 15:37:17 -0600

    forgot dis

--- assn2/server_s.c
+++ assn2/server_s.c
@@ -23,6 +23,19 @@
 #include <unistd.h>
 
 #include "helpers.h"
+
+/* we use this structure to keep track of each connection to us */
+struct con {
+	int sd; 	/* the socket for this connection */
+	int state; 	/* the state of the connection */
+	struct sockaddr_in sa; /* the sockaddr of the connection */
+	size_t  slen;   /* the sockaddr length of the connection */
+	char *buf;	/* a buffer to store the characters read in */
+	char *bp;	/* where we are in the buffer */
+	size_t bs;	/* total size of the buffer */
+	size_t bl;	/* how much we have left to read/write */
+};
+
 #define MAXCONN 256 /* Max # of connections possible */
 struct con connections[MAXCONN];
 
